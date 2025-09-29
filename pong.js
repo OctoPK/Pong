a82c183112959680b4759a8a42971bc97d894223
OctoPK <armand@biguey.com>
2025-09-29 18:53:33 +0200

    add moving ball

--- pong.js
+++ pong.js
@@ -12,10 +12,27 @@ let yPaddle = canva.height - 12;
 let xBall = canva.width / 2;
 let yBall = canva.height - 20;
 let speedPaddle = 10;
+let moveX;
+let moveY;
+let speed = 1.4;
+let gameRunning = 0;
 
 bt_new_partie.addEventListener("click", () => {
+    cancelAnimationFrame(rafId);
+    resetGame();
     start = Date.now();
     score.textContent = "Score : 0 s";
+    moveX = Math.random();
+    speed = 1.4;
+    gameRunning = 1;
+    if (moveX > 0.5) {
+        xBall += moveX * speed;
+    } else {
+        moveX *= -1;
+        xBall += moveX * speed;
+    }
+    moveY = -1 * speed;
+    loop();
 });
 
 function updateScore() {
@@ -45,15 +62,57 @@ function drawBall() {
 }
 
 function loop() {
+    if (!gameRunning) return;
     ctx.clearRect(0, 0, canva.width, canva.height);
+    moveBall();
     updateScore();
     drawPaddle();
     drawBall();
     rafId = requestAnimationFrame(loop);
 }
 
-loop();
+function moveBall() {
+    if (xBall + 6 >= canva.width) {
+        moveX = Math.random() * -1;
+        if (speed <= 7) speed *= 1.05;
+    }
+    if (xBall - 6 <= 0) {
+        moveX = Math.random();
+        if (speed <= 7) speed *= 1.05;
+    }
+    if (yBall - 6 <= 0) {
+        moveY = 1;
+        if (speed <= 7) speed *= 1.05;
+    }
+    if (yBall + 6 >= canva.height) {
+        gameRunning = 0;
+        gameOver();
+        return;
+    }
+    if (yBall + 6 >= yPaddle && xBall >= xPaddle && xBall <= xPaddle + 70 && moveY > 0) {
+        moveY = -1;
+        if (moveX > 0) moveX = Math.random();
+        else moveX = Math.random() * -1;
+        if (speed <= 7) speed *= 1.05;
+    }
+    xBall += moveX * speed;
+    yBall += moveY * speed;
+}
 
+function movePaddleLeft() {
+    if (xPaddle > 0) xPaddle -= speedPaddle;
+}
+
+function movePaddleRight() {
+    if (xPaddle + 70 < canva.width) xPaddle += speedPaddle;
+}
+
+bt_move_left.addEventListener("click", () => {
+    movePaddleLeft();
+});
+bt_move_right.addEventListener("click", () => {
+    movePaddleRight();
+});
 document.addEventListener("keydown", (e) => {
     switch (e.key) {
         case "ArrowLeft":
@@ -65,16 +124,22 @@ document.addEventListener("keydown", (e) => {
         default:
             break;
     }
-    drawPaddle();
 });
 
-function movePaddleLeft() {
-    if (xPaddle > 0) xPaddle -= speedPaddle;
+drawBall();
+drawPaddle();
+
+function resetGame() {
+    xBall = canva.width / 2;
+    yBall = canva.height - 20;
+    xPaddle = canva.width / 2 - 35;
+    yPaddle = canva.height - 12;
 }
 
-function movePaddleRight() {
-    if (xPaddle + 70 < canva.width) xPaddle += speedPaddle;
+function gameOver() {
+    cancelAnimationFrame(rafId);
+    resetGame();
+    ctx.clearRect(0, 0, canva.width, canva.height);
+    drawBall();
+    drawPaddle();
 }
-
-bt_move_left.addEventListener("click", movePaddleLeft());
-bt_move_right.addEventListener("click", movePaddleRight());
